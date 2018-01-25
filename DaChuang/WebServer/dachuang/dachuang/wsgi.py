d267d0c9c8db91938a8c312cb82c3ad0a5a663d7
dachuang <dachuangroot@163.com>
2018-01-25 23:50:21 +0800

    alter the wsgi.py to test

--- DaChuang/WebServer/dachuang/dachuang/wsgi.py
+++ DaChuang/WebServer/dachuang/dachuang/wsgi.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+sys.path.append("/var/www/html/dachuang")
+
+sys.path.append('/var/www/html')
+
+sys.path.append('/var/www/html/dachuang/dcweb')
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dachuang.settings")
